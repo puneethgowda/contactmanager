0becc1cd72fede306683486b6c032502f35d9bfd
puneethpgowda <puneeth.gowda@axisrooms.com>
2018-09-10 01:22:20 +0530

    initial commit

---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { Consumer } from '../../Context';
+import axios from 'axios';
+
+ class Contact extends Component {
+     state = {
+         showContactInfo: false
+     };
+
+      onDeleteClick = async(id, dispatch) =>{
+          try {
+            await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+            dispatch({type: 'DELETE_CONTACT', payload: id});
+          } catch (error) {
+            dispatch({type: 'DELETE_CONTACT', payload: id});
+          }
+
+      
+     }
+   
+  render() {
+    const {id, name, email, phone} = this.props.contact;
+    const {showContactInfo} = this.state;
+    return (
+      
+      <Consumer>
+          {value =>{
+            const {dispatch} = value; 
+            return(
+                <div className="card card-body mb-3">
+          <h4>
+          {name} <i 
+          onClick={()=> 
+          this.setState({showContactInfo: !this.state.showContactInfo})} 
+          className="fas fa-sort-down"
+          style={{cursor: 'pointer'}}>
+          </i>
+          <i
+          onClick={this.onDeleteClick.bind(this, id, dispatch)}
+          className="fas fa-times"
+          style={{cursor: 'pointer', color:'red',float:'right'}}>
+          </i>
+          <Link to={`/contact/edit/${id}`} >
+          <i 
+            className="fas fa-pencil-alt"
+            style={{
+                cursor:'pointer',
+                float:'right',
+                color:'black',
+                marginRight:'1rem'
+            }}
+            ></i>
+          </Link>
+          </h4>
+          {showContactInfo ? ( <ul className="list-group">
+              <li className="list-group-item">Email: {email}</li>
+              <li className="list-group-item">Phone: {phone}</li>
+          </ul>) : null}
+        
+        </div>
+            )
+          }}
+      </Consumer>
+    )
+  }
+}
+
+export default Contact;
